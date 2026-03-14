7808a3e7c91f50cf63c72c4ed00bf8f115fa3cee
OpenChromeDev <carolcst@hotmail.it>
2026-03-14 18:23:33 +0100

    Update build script for OpenChrome ISO creation

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-echo "--- INIZIO COSTRUZIONE OPENCHROME ---"
+echo "--- COSTRUZIONE STRUTTURA OPENCHROME ---"
 
-# 1. Installazione pacchetti
-apt-get update
-apt-get install -y lxqt sddm network-manager chromium-browser --no-install-recommends
+# 1. Prepariamo le cartelle per la ISO vera
+mkdir -p iso_content/casper
+mkdir -p iso_content/boot/grub
 
-# 2. Personalizzazione
-echo "OpenChrome-Flex" > /etc/hostname
-echo "OpenChrome OS 1.0 - Creato da Gabriele" > /etc/issue
+# 2. Creiamo un "mini-sistema" di prova
+# (In un OS vero qui andrebbe il filesystem compresso)
+echo "Configurazione OpenChrome Flex di Gabriele" > iso_content/casper/filesystem.manifest
 
-# 3. CREAZIONE ISO (Il pezzo mancante)
-# Creiamo una cartella che diventerà la nostra ISO
-mkdir -p iso_root
-echo "Benvenuti in OpenChrome OS" > iso_root/readme.txt
+# 3. Creazione della ISO
+# Usiamo impostazioni più serie per renderla "avviabile"
+xorriso -as mkisofs \
+    -V "OpenChrome_Flex" \
+    -o openchrome_flex.iso \
+    -J -joliet-long -r \
+    iso_content/
 
-# Usiamo xorriso per creare il file .iso
-xorriso -as mkisofs -o openchrome_flex.iso iso_root/
-
-echo "--- ISO CREATA CON SUCCESSO ---"
+echo "--- ISO GENERATA ---"
